573d74617d41cf40e4bce50d0351cd07519357bd
Izabelle Santos do Nascimento <izabelle.sdn@gmail.com>
2022-05-24 17:00:33 -0300

    inserir, listar, inicio de atualizar produtos

--- includes/funcoes-produtos.php
+++ includes/funcoes-produtos.php
@@ -3,8 +3,12 @@
 require "conecta.php";
 
 function lerProdutos($conexao){
-    $sql = "SELECT id, nome, preco, quantidade, descricao, fabricantes_id
-    FROM produtos ORDER BY nome";
+    // $sql = "SELECT id, nome, preco, quantidade, descricao, fabricantes_id
+    // FROM produtos ORDER BY nome";
+
+    $sql = "SELECT produtos.id, produtos.nome AS produto, produtos.preco, produtos.quantidade, produtos.descricao, fabricantes.nome AS fabricante
+    FROM produtos INNER JOIN fabricantes
+    ON produtos.fabricantes_id = fabricantes.id ORDER BY produto";
 
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     
@@ -20,19 +24,19 @@ function lerProdutos($conexao){
 //teste
 // var_dump(lerProdutos($conexao));
 
-function inserirProduto($conexao, $nome){
-    $sql = "INSERT INTO produtos(nome) VALUES('$nome')";
+function inserirProduto($conexao, $nome, $preco, $quantidade, $descricao, $fabricanteId){
+    $sql = "INSERT INTO produtos(nome, preco, quantidade, descricao, fabricantes_id) VALUES('$nome', $preco, $quantidade, '$descricao', $fabricanteId)";
     mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
 
 function lerUmProduto($conexao, $id){
-    $sql = "SELECT id, nome FROM produtos WHERE id = $id";
+    $sql = "SELECT id, nome, preco, quantidade, descricao, fabricantes_id FROM produtos WHERE id = $id";
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     return mysqli_fetch_assoc($resultado);
 }
 
-function atualizarProduto($conexao, $id, $nome){
-    $sql = "UPDATE produtos SET nome = '$nome' WHERE id = $id";
+function atualizarProduto($conexao, $id, $nome, $preco, $quantidade, $descricao){
+    $sql = "UPDATE produtos SET nome = '$nome', preco = $preco, quantidade = $quantidade, descricao = '$descricao' WHERE id = $id";
     mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
 
